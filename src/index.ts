e6b4d0f3e76e1360eaff0404fa99bc2f0375d4b6
camposAaron <campos.aaron.15@gmail.com>
2022-05-02 13:35:02 -0600

    01-Creando un observable

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,31 @@
+import { Observable } from 'rxjs';
 
 
-console.log('Hola Mundo!');
+// const obs$ = Observable.create()
+
+const obs$ = new Observable( subs => {
+
+    subs.next('hola');
+    subs.next('mundo');
+    subs.next('desde');
+    subs.next('marte');
+
+    subs.complete()
+    
+    
+    subs.next('hola');
+    subs.next('mundo');
+    subs.next('desde');
+    subs.next('marte');
+});
+
+
+obs$.subscribe( console.log );
+
+
+
+
+
 
 
 
